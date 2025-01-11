0bfe117115a024b4e62c06376fa4dd9dfb37b7da
Aleksandr Arofikin <sashaaro@gmail.com>
2025-01-11 09:02:09 +0300

    test register

--- account/src/test/test.rs
+++ account/src/test/test.rs
@@ -3,7 +3,9 @@ use std::sync::Arc;
 #[cfg(test)]
 mod tests {
     use super::*;
-    use actix_web::{test, web, App};
+    use actix_web::{test, web, App, HttpMessage};
+    use actix_web::http::header::ContentType;
+    use sqlx::Executor;
     use crate::application::account::Application;
     use crate::{config_app, infra};
     use crate::infra::auth::AuthManager;
@@ -23,5 +25,24 @@ mod tests {
         let req = test::TestRequest::post().uri("/").to_request();
         let resp = test::call_service(&app, req).await;
         assert_eq!(resp.status().as_u16(), 404);
+    }
+
+    #[actix_web::test]
+    async fn test_register() {
+        let pool = db::pg().await;
+        pool.execute("truncate \"user\" cascade").await.unwrap();
+
+        let app = test::init_service(
+            App::new().configure(config_app(pool.clone()))
+        )
+            .await;
+
+        let req = test::TestRequest::post()
+            .insert_header(ContentType::json())
+            .set_payload("{\"name\": \"alex\", \"password\": \"123\"}")
+            .uri("/register")
+            .to_request();
+        let resp = test::call_service(&app, req).await;
+        assert_eq!(resp.status().as_u16(), 200);
     }
 }
